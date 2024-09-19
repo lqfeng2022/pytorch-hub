60428ce6a89b9bc5ae645b906911aa151bada39a
lqfeng2022 <simonlqf@hotmail.com>
2024-09-19 12:04:42 +0800

    Change the book_img url

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -7,8 +7,8 @@ import {
   Text,
   useBreakpointValue,
 } from '@chakra-ui/react';
-import book_img from '../src/assets/book_cover.jpeg';
-import book_img_2 from '../src/assets/book_cover_2.jpeg';
+import book_img from '../public/covers/book_cover.jpeg';
+import book_img_2 from '../public/covers/book_cover_2.jpeg';
 import { useState } from 'react';
 import BookList from './components/BookList';
 
